839e46f4eeed40be4b90e3c644fe48aa1c4d406e
naufalrzld <naufalrzld@gmail.com>
2019-01-23 17:40:37 +0700

    Init Commit

---
+++ app/src/main/java/com/naufalrzld/list/MainActivity.java
@@ -0,0 +1,29 @@
+package com.naufalrzld.list;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Integer> listAngka = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView lvItem = findViewById(R.id.lv_item);
+
+        for (int i=0; i<100; i++) {
+            listAngka.add(i+1);
+        }
+
+        ArrayAdapter<Integer> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listAngka);
+
+        lvItem.setAdapter(adapter);
+    }
+}
